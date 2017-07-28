03cc68e64d61389451b6944432bd23f6e9af3bdb
Ilya <vik.kor.1917@mail.ru>
2017-07-28 14:36:27 +0300

    Change Add method

--- Calcul/Class1.cs
+++ Calcul/Class1.cs
@@ -10,7 +10,7 @@ namespace Calcul
     {
         public double Add(double num1, double num2)
         {
-            return num1 + num2;
+            return num2 + num1;
         }
         public double Square(double num)
         {
