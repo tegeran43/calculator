7868e67dd9724c5a3299b8776cf0434233ef4413
Ilya <vik.kor.1917@mail.ru>
2017-07-28 14:29:35 +0300

    Set initial catalog

---
+++ Calcul/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcul
+{
+    public class Class1
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Square(double num)
+        {
+            return num * num;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calcul;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            Console.WriteLine(obj.Add(3, 7));
+            
+
+
+        }
+    }
+}
